2b3bca6ff0b388d69d02c09cdcf0ef609ac59b22
aassarattanakul <aassarattanakul@andrew-mac.sw-network.com>
2010-01-04 09:47:50 -0600

    Added tests for base carrier values for FedEx, Shipwire and UPS.

--- test/unit/base_test.rb
+++ test/unit/base_test.rb
@@ -3,6 +3,24 @@ require File.dirname(__FILE__) + '/../test_helper'
 class BaseTest < Test::Unit::TestCase
   include ActiveMerchant::Shipping
 
+  def test_get_fedex
+    assert_equal FedEx, Base.carrier('fedex')
+    assert_equal FedEx, Base.carrier('FEDEX')
+    assert_equal FedEx, Base.carrier(:fedex)
+  end
+
+  def test_get_shipwire
+    assert_equal Shipwire, Base.carrier('shipwire')
+    assert_equal Shipwire, Base.carrier('SHIPWIRE')
+    assert_equal Shipwire, Base.carrier(:shipwire)
+  end
+
+  def test_get_ups
+    assert_equal UPS, Base.carrier('ups')
+    assert_equal UPS, Base.carrier('UPS')
+    assert_equal UPS, Base.carrier(:ups)
+  end
+
   def test_get_usps_by_string
     assert_equal USPS, Base.carrier('usps')
     assert_equal USPS, Base.carrier('USPS')
@@ -11,7 +29,7 @@ class BaseTest < Test::Unit::TestCase
   def test_get_usps_by_name
     assert_equal USPS, Base.carrier(:usps)
   end
-  
+
   def test_get_unknown_carrier
     assert_raise(NameError){ Base.carrier(:polar_north) }
   end
